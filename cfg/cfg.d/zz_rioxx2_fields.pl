1c2e2b9fce9c7b95c2f27801e95c13b1740fdcaa
Timothy Miles-Board <timothy.miles-board@london.ac.uk>
2014-08-23 12:05:10 +0100

    #1 Make field naming consistent with spec, dcterms:dateAccepted can be just YYYY; match capitalisation of rioxxterms:version

--- cfg/cfg.d/zz_rioxx2_fields.pl
+++ cfg/cfg.d/zz_rioxx2_fields.pl
@@ -12,22 +12,22 @@ $c->add_dataset_field(
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_date_accepted", type => "date", min_resolution => "month", required => 1 }
+	{ name => "rioxx2_dateaccepted", type => "date", min_resolution => "year", required => 1 }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_freetoread", type => "compound", fields => [
-		{ sub_name => "start", type => "date" },
-		{ sub_name => "end", type => "date" }
+	{ name => "rioxx2_free_to_read", type => "compound", fields => [
+		{ sub_name => "start_date", type => "date", min_resolution => "day" },
+		{ sub_name => "end_date", type => "date", min_resolution => "day" }
 	] }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_license", type => "compound", fields => [
-		{ sub_name => "url", type => "url" },
-		{ sub_name => "start", type => "date" }
+	{ name => "rioxx2_license_ref", type => "compound", fields => [
+		{ sub_name => "license_ref", type => "url" },
+		{ sub_name => "start_date", type => "date" }
 	], required => 1 }
 );
 
@@ -39,7 +39,7 @@ $c->add_dataset_field(
 $c->add_dataset_field(
 	"eprint",
 	{ name => "rioxx2_project", type => "compound",	fields => [
-		{ sub_name => "id", type => "text" },
+		{ sub_name => "project", type => "text" },
 		{ sub_name => "funder_name", type => "text" },
 		{ sub_name => "funder_id", type => "url" }
 	], required => 1, multiple => 1 }
@@ -52,11 +52,10 @@ $c->add_dataset_field(
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_type", type => "set",
-	options => [qw( book book_chapter book_edited conference_item journal_article manual monograph policy_report tech_report tech_standard thesis other consultancy_report working_paper 	)], required => 1, multiple => 0 }# N.B. Profile says this is a "one or more"
+	{ name => "rioxx2_type", type => "set", options => [qw( book book_chapter book_edited conference_item journal_article manual monograph policy_report tech_report tech_standard thesis other consultancy_report working_paper )], required => 1, multiple => 1 }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_version", type => "set", options => [qw( ao smur am p vor cvor evor na )], required => 1 }
+	{ name => "rioxx2_version", type => "set", options => [qw( AO SMUR AM P VoR CVoR EVoR NA )], required => 1 }
 );
