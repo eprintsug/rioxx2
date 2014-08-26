2a75511685536a6f0f3127468f70ee496294e2ec
Timothy Miles-Board <timothy.miles-board@london.ac.uk>
2014-08-26 01:36:13 +0100

    Create a virtual field for every rioxx field - use rioxx_value property to specify a derived value; to add a field to the workflow an additional (non-virtual) *_input field must also be created which will be used to store user-entered value

--- cfg/cfg.d/zz_rioxx2_fields.pl
+++ cfg/cfg.d/zz_rioxx2_fields.pl
@@ -1,23 +1,153 @@
 # New EPrint Fields for RIOXX2
 
+$c->{rioxx2}->{type_map} = {
+	article		=> 'Journal Article/Review',
+	book_section	=> 'Book chapter',
+	monograph	=> 'Monograph',
+	conference_item	=> 'Conference Paper/Proceeding/Abstract',
+	book		=> 'Book',
+	thesis		=> 'Thesis',
+};
+
+$c->{rioxx2}->{content_map} = {
+	draft		=> "AO",
+	submitted	=> "SMUR",
+	accepted	=> "AM",
+	published	=> "P",
+};
+
+# rioxx2 virtual fields
+
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_coverage", type => "text", multiple => 1 }
+	{ name => "rioxx2_coverage", type => "rioxx2", rioxx2_required => "optional", rioxx2_ns => "dc"  }
+);
+
+# TODO strip HTML
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_description", type => "rioxx2", rioxx2_value => sub { $_[0]->value( "abstract" ) }, rioxx2_required => "recommended", rioxx2_ns => "dc" }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_language", type => "namedset", input_rows => 1, set_name => "languages", multiple => 1, required => 1 }
+	{ name => "rioxx2_format", type => "rioxx2", rioxx2_value => sub { $_[1] && $_[1]->value( "mime_type" ) }, rioxx2_required => "recommended", rioxx2_ns => "dc" }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_dateAccepted", type => "date", min_resolution => "year", required => 1 }
+	{ name => "rioxx2_identifier", type => "rioxx2", rioxx2_value => sub { $_[1] && $_[1]->get_url }, rioxx2_required => "mandatory", rioxx2_ns => "dc" }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_free_to_read", type => "compound", fields => [
+	{ name => "rioxx2_language", type => "rioxx2", rioxx2_value => sub { $_[1] && $_[1]->value( "language" ) }, rioxx2_required => "mandatory", rioxx2_ns => "dc" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_publisher", type => "rioxx2", rioxx2_value => sub { $_[0]->value( "publisher" ) }, rioxx2_required => "recommended", rioxx2_ns => "dc" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_relation", type => "rioxx2", rioxx2_value => sub { $_[0]->value( "related_url_url" ) }, rioxx2_required => "optional", rioxx2_ns => "dc" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_source", type => "rioxx2", rioxx2_value => "rioxx2_source", rioxx2_required => "mandatory", rioxx2_ns => "dc" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_subject", type => "rioxx2", rioxx2_value => sub { $_[0]->value( "subjects" ) }, rioxx2_required => "recommended", rioxx2_ns => "dc" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_title", type => "rioxx2", rioxx2_value => sub { $_[0]->value( "title" ) }, rioxx2_required => "mandatory", rioxx2_ns => "dc" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_dateAccepted", type => "rioxx2", rioxx2_required => "mandatory", rioxx2_ns => "dcterms" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_free_to_read", type => "rioxx2", rioxx2_value => "rioxx2_free_to_read", rioxx2_required => "optional", rioxx2_ns => "tbc" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	# TODO rioxx2_value: derive from document.license + document.date_embargo
+	# TODO rioxx2_validate: license_ref must be HTTP URL, must include start_date
+	{ name => "rioxx2_license_ref", type => "rioxx2", rioxx2_required => "mandatory", rioxx2_ns => "tbc" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_apc", type => "rioxx2", rioxx2_required => "optional", rioxx2_ns => "rioxxterms" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_author", type => "rioxx2", rioxx2_value => "rioxx2_author", rioxx2_required => "mandatory", rioxx2_ns => "rioxxterms" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_contributor", type => "rioxx2", rioxx2_value => "rioxx2_contributor", rioxx2_required => "optional", rioxx2_ns => "rioxxterms" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_project", type => "rioxx2", rioxx2_value => "rioxx2_project", rioxx2_required => "mandatory", rioxx2_ns => "rioxxterms" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_publication_date", type => "rioxx2", rioxx2_value => sub { ( !$_[0]->is_set( "date_type" ) || $_[0]->value( "date_type" ) eq "published" ) && $_[0]->value( "date" ) }, rioxx2_required => "optional", rioxx2_ns => "rioxxterms" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_type", type => "rioxx2", rioxx2_value => sub { $_[0]->repository->config( "rioxx2", "type_map", $_[0]->get_type ) || "other" }, rioxx2_required => "mandatory", rioxx2_ns => "rioxxterms" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_version", type => "rioxx2", rioxx2_value => sub { $_[1] && $_[1]->repository->config( "rioxx2", "content_map", $_[1]->value( "content" ) ) || "NA" }, rioxx2_required => "mandatory", rioxx2_ns => "rioxxterms" }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	# TODO check id_number contains a DOI
+	# TODO rioxx2_validate: must be a HTTP URL
+	{ name => "rioxx2_version_of_record", type => "rioxx2", rioxx2_value => sub { ( $_[0]->exists_and_set( "doi" ) && $_[0]->value( "doi" ) ) || $_[0]->value( "id_number" ) }, rioxx2_required => "mandatory", rioxx2_ns => "rioxxterms" }
+);
+
+# overrides
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_coverage_input", type => "text", multiple => 1 }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_language_input", type => "namedset", input_rows => 1, set_name => "languages", multiple => 1, required => 1 }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_dateAccepted_input", type => "date", min_resolution => "year", required => 1 }
+);
+
+$c->add_dataset_field(
+	"eprint",
+	{ name => "rioxx2_free_to_read_input", type => "compound", fields => [
 		{ sub_name => "free_to_read", type => "boolean" },
 		{ sub_name => "start_date", type => "date", min_resolution => "day" },
 		{ sub_name => "end_date", type => "date", min_resolution => "day" }
@@ -26,7 +156,7 @@ $c->add_dataset_field(
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_license_ref", type => "compound", fields => [
+	{ name => "rioxx2_license_ref_input", type => "compound", fields => [
 		{ sub_name => "license_ref", type => "url" },
 		{ sub_name => "start_date", type => "date" }
 	], required => 1 }
@@ -34,12 +164,12 @@ $c->add_dataset_field(
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_apc", type => "set", options => [qw( paid partially_waived fully_waived not_charged not_required unknown )] }
+	{ name => "rioxx2_apc_input", type => "set", options => [ "paid", "partially waived", "fully waived", "not charged", "not required", "unknown" ] }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_project", type => "compound",	fields => [
+	{ name => "rioxx2_project_input", type => "compound",	fields => [
 		{ sub_name => "project", type => "text" },
 		{ sub_name => "funder_name", type => "text" },
 		{ sub_name => "funder_id", type => "url" }
@@ -48,15 +178,86 @@ $c->add_dataset_field(
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_publication_date", type => "text" }
+	{ name => "rioxx2_publication_date_input", type => "text" }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_type", type => "set", options => [qw( book book_chapter book_edited conference_item journal_article manual monograph policy_report tech_report tech_standard thesis other consultancy_report working_paper )], required => 1, multiple => 1 }
+	{ name => "rioxx2_type_input", type => "set", options => [ "Book", "Book chapter", "Book edited", "Conference Paper/Proceeding/Abstract", "Journal Article/Review", "Manual/Guide", "Monograph", "Policy briefing report", "Technical Report", "Technical Standard", "Thesis", "Other", "Consultancy Report", "Working paper" ], required => 1, multiple => 1 }
 );
 
 $c->add_dataset_field(
 	"eprint",
-	{ name => "rioxx2_version", type => "set", options => [qw( AO SMUR AM P VoR CVoR EVoR NA )], required => 1 }
+	{ name => "rioxx2_version_input", type => "set", options => [qw( AO SMUR AM P VoR CVoR EVoR NA )], required => 1 }
 );
+
+# more complex mappings
+
+$c->{rioxx2_source} = sub {
+	my ( $eprint ) = @_;
+
+	return unless $eprint->get_type eq "article" || $eprint->get_type eq "book_section" || $eprint->get_type eq "conference_item";
+
+	for( qw( issn isbn publication book_title event_title ) )
+	{
+		next unless $eprint->is_set( $_ );
+		return $eprint->value( $_ );
+	}
+};
+
+$c->{rioxx2_free_to_read} = sub {
+	my( $eprint, $document ) = @_;
+
+	return unless $document;
+	return { free_to_read => 1 } if $document->is_set( "security" ) && $document->value( "security" ) eq "public";
+	return { free_to_read => 1, start_date => $document->value( "date_embargo" ) } if $document->is_set( "date_embargo" );
+};
+
+$c->{rioxx2_author} = sub {
+	my( $eprint ) = @_;
+
+	my @authors;
+	for( @{ $eprint->value( "creators_name" ) } )
+	{
+		push @authors, EPrints::Utils::make_name_string( $_ );
+	}
+	push @authors, @{ $eprint->value( "corp_creators" ) };
+
+	return \@authors;
+};
+
+$c->{rioxx2_contributor} = sub {
+	my( $eprint ) = @_;
+
+	my @contributors;;
+	for( @{ $eprint->value( "editors_name" ) }, @{ $eprint->value( "contributors_name" ) } )
+	{
+		push @contributors, EPrints::Utils::make_name_string( $_ );
+	}
+
+	return \@contributors;
+};
+
+$c->{rioxx2_project} = sub {
+	my( $eprint ) = @_;
+
+	return unless $eprint->is_set( "funders" ) && $eprint->is_set( "projects" );
+
+	my @p = @{ $eprint->value( "projects" ) };
+	my @f = @{ $eprint->value( "funders" ) };
+
+	while( scalar @p < scalar @f )
+	{
+		push @p, $p[$#p];
+	}
+
+	my @projects;
+	for( my $i = 0; $i < scalar @p; $i++ )
+	{
+		push @projects, {
+			project => $p[$i],
+			funder_name => ( $i > $#f ? $f[$#f] : $f[$i] ),
+		};
+	}
+	return \@projects;
+};
---
+++ lib/plugins/EPrints/MetaField/Rioxx2.pm
@@ -0,0 +1,66 @@
+package EPrints::MetaField::Rioxx2;
+
+@ISA = qw( EPrints::MetaField );
+
+use EPrints::Const qw( :metafield );
+
+use strict;
+
+sub is_virtual { 1 }
+
+sub get_value
+{
+	my( $self, $object, $ignore_override ) = @_;
+
+	if( !$ignore_override && $object->exists_and_set( $self->name . "_input" ) )
+	{
+		return $object->value( $self->name . "_input" );
+	}
+
+	# will return undef if property not defined
+	return $self->call_property( "rioxx2_value", $object, ($object->get_all_documents)[0] ); # TODO remove implicit assumption that object is a DataObj::EPrint
+}
+
+sub render_value
+{
+	my( $self, $repo, undef, undef, undef, $object, $ignore_override ) = @_;
+
+	my $value = $self->get_value( $object, $ignore_override );
+
+	{
+		use Data::Dumper;
+		$Data::Dumper::Terse = 1;
+		$Data::Dumper::Indent = 0;
+		return $repo->xml->create_text_node( Dumper( $value ) );
+	}
+}
+
+sub validate
+{
+	my( $self, $repo, $value, $object ) = @_;
+
+	my @problems = $self->SUPER::validate( $repo, $value, $object );
+
+	if( $self->has_property( "rioxx2_validate" ) )
+	{
+		push @problems, $self->call_property( "rioxx2_validate", $repo, $value, $object );
+	}
+
+	return @problems;
+}
+
+sub get_property_defaults
+{
+	my( $self ) = @_;
+
+	my %defaults = $self->SUPER::get_property_defaults;
+
+	$defaults{rioxx2_value} = EP_PROPERTY_UNDEF;
+	$defaults{rioxx2_validate} = EP_PROPERTY_UNDEF;
+	$defaults{rioxx2_required} = EP_PROPERTY_REQUIRED;
+	$defaults{rioxx2_ns} = EP_PROPERTY_REQUIRED;
+
+	return %defaults;
+}
+
+1;
