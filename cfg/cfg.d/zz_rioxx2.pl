76a725d4ead1330857769606a49fb2156e274126
Timothy <timothy.miles-board@london.ac.uk>
2014-10-10 15:09:37 +0100

    Let the reports epm enable its own plugins; tidy up

--- cfg/cfg.d/zz_rioxx2.pl
+++ cfg/cfg.d/zz_rioxx2.pl
@@ -1,24 +1,12 @@
-$c->{plugins}{"Screen::Report"}{params}{disable} = 0;
-$c->{plugins}{"Screen::Report::Rioxx2"}{params}{disable} = 0;
-$c->{plugins}{"Screen::Report::Rioxx2::Articles"}{params}{disable} = 0;
-#$c->{plugins}{"Screen::Report::Example"}{params}{disable} = 0;
-#$c->{plugins}{"Screen::Report::Example::Articles"}{params}{disable} = 0;
-#$c->{plugins}{"Screen::Report::Example::ConferenceItems"}{params}{disable} = 0;
 
+# Enable core RIOXX2 plugins
 $c->{plugins}{'Export::RIOXX2'}{params}{disable} = 0;
-$c->{plugins}{"Export::Report"}{params}{disable} = 0;
-$c->{plugins}{"Export::Report::CSV"}{params}{disable} = 0;
-#$c->{plugins}{"Export::Report::CSV::Example"}{params}{disable} = 0;
-$c->{plugins}{"Export::Report::CSV::Rioxx2"}{params}{disable} = 0;
-
 $c->{plugins}{"Screen::EPrint::RIOXX2"}{params}{disable} = 0;
-
 $c->{plugins}{'InputForm::Component::Field::Rioxx2'}{params}{disable} = 0;
 
-# New report role for the admin user
-push @{$c->{user_roles}->{admin}}, qw{
-        +report
-};
+# Enable optional RIOXX2 plugins for reporting framework (https://github.com/eprints/reports)
+$c->{plugins}{"Screen::Report::Rioxx2"}{params}{disable} = 0;
+$c->{plugins}{"Screen::Report::Rioxx2::Articles"}{params}{disable} = 0;
+$c->{plugins}{"Export::Report::CSV::Rioxx2"}{params}{disable} = 0;
 
 $c->{fundref_csv_file} = $c->{"config_path"}."/autocomplete/funderNames";
-
